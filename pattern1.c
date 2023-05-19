806531d14835d9e30128218141a68cd0831f2fd5
ifthekar <sajalcse2018@gmail.com>
2023-05-19 08:46:50 +0600

    pattern 1 program create

---
+++ pattern1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int num,rows,cols;
+    printf("Enter Number of Rows= ");
+    scanf("%d",&num);
+
+    for(rows=1;rows<=num;rows++){
+
+        for(cols=1;cols<=rows;cols++){
+
+            printf("%d ",cols);
+        }
+        printf("\n");
+    }
+
+
+return 0;
+}
