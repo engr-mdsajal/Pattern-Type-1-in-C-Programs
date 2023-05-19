abc3c841b8fd56bba1b6ebf94f288ec0850b5860
ifthekar <sajalcse2018@gmail.com>
2023-05-19 12:26:17 +0600

    * pattern print

---
+++ pattern10.c
@@ -0,0 +1,17 @@
+int main(){
+
+    int num,rows,cols;
+    printf("Enter Number of Rows which You Want to patterns= ");
+    scanf("%d",&num);
+
+    for(rows=1;rows<=num;rows++){
+
+        for(cols=1;cols<=rows;cols++){
+
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+return 0;
+}
